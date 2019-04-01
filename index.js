4d0449d642f524cc4d152b3072db54a27c77afce
Nathan Miranda <nate845@gmail.com>
2019-04-01 23:56:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ window.addEventListener("load", () => {
     // ex: howManyCharacters = 10
     // we want to loop and make a random letter every time
     // start from 0, finish at 9
+    let startingString = ''
     for (let i = 0; i < howManyCharacters; i++) {
       // we pick a random letter 
       const randomIndex = Math.floor(Math.random() * 26)
-      console.log(lowerCaseLetters[randomIndex])
-      
+      startingString += lowerCaseLetters[randomIndex]
     }
+    console.log(startingString)
     
     
     
