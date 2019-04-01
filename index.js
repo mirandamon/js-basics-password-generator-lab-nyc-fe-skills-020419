b74824816017136f44047be45ec1d974d4a9b6c4
Nathan Miranda <nate845@gmail.com>
2019-04-01 23:52:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ window.addEventListener("load", () => {
     for (let i = 0; i < howManyCharacters; i++) {
       // we pick a random letter 
       console.log(Math.floor(Math.random() * 26))
-      // []
+      // myArray[0]
+      
     }
     
     
