d6edd7c4d54091d9eae90a98b36cce09e3ad7262
Nathan Miranda <nate845@gmail.com>
2019-04-01 23:33:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 window.addEventListener("load", () => {
   //start your code here
+  
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+  
+  function onSubmit(event) {
+    event.preventDefault()
+    // get the thing that was entered
+    const input = document.getElementById('password-length')
+    console.log(input.value)
+  }
+  
+  document.addEventListener('submit', onSubmit)
 });
